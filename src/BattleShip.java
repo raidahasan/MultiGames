4b6118cba1d7c80cb681569cbf6f724955a6fb65
BT_1N3_26 <@gmail.com>
2024-03-15 10:40:41 -0400

    add getter method and created instances

--- src/BattleShip.java
+++ src/BattleShip.java
@@ -1,14 +1,25 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class BattleShip {
     private String player1;
     private String player2;
-    private int score1;
-    private int score2;
-    private boolean hit;
-    private boolean missed;
+    private int score1 = 0;
+    private int score2 = 0;
+    private boolean hit = false;
+    private boolean missed = false;
+    private ArrayList<String> positions;
 
     public BattleShip(String player1, String player2){
         this.player1 = player1;
         this.player2 = player2;
-
+        positions = new ArrayList<>();
     }
+
+    public ArrayList<String> getPositions() {
+        return positions;
+    }
+
+
+    public void placeShip(int x, String)
 }
--- src/BattleShipSetUp.java
+++
@@ -1,9 +0,0 @@
-public class BattleShipSetUp extends BattleShip {
-    private int boardSize;
-    private int numShips;
-    public BattleShipSetUp(String player1,String player2, int boardSize, int numShips){
-        super(player1, player2);
-        this.boardSize = boardSize;
-        this.numShips = numShips;
-    }
-}
