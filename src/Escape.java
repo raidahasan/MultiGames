f704af347f0eb6839ad213a478db879edcf9283d
Raida <raidah@nycstudents.net>
2024-04-07 01:33:32 -0400

    it works!!!!!!

--- src/Escape.java
+++ src/Escape.java
@@ -8,6 +8,7 @@ public class Escape {
     Obstacle A = new Obstacle();
     Obstacle B = new Obstacle();
     private boolean hit = false;
+    int playerInput =0;
     int row;
     int col;
     PointsPlayer p = new PointsPlayer();
@@ -25,27 +26,34 @@ public class Escape {
         insertObstacle(X);
         while(!hit) {
             printEsc();
-            updateObstacle(X);
             playerTurn();
         }
         printEsc();
     }
 
     public void insertObstacle(Obstacle obstacle){
-        obstacle.setColumn((int)(Math.random()*3)+22);
-        obstacle.setRow((int)(Math.random()*4)+1);
+        obstacle.setColumn(24);  //((int)(Math.random()*3)+22)
+        obstacle.setRow((int)(Math.random()*5));
         board[obstacle.getRow()][obstacle.getColumn()] = obstacle;
 
     }
 
     public void updateObstacle(Obstacle obstacle){
         board[obstacle.getRow()][obstacle.getColumn()] = new Space("_");
-        if(obstacle.getColumn()!=0 && obstacle.getRow()!=0){
+        if(obstacle.getColumn()!=0){
             obstacle.setColumn(obstacle.getColumn()-1);
             board[obstacle.getRow()][obstacle.getColumn()] = obstacle;
         }else{
             board[obstacle.getRow()][obstacle.getColumn()] = new Space("_");
+            insertObstacle(obstacle);
         }
+    }
+    public void updateAllObstacles(){
+        updateObstacle(X);
+        updateObstacle(Y);
+        updateObstacle(Z);
+        updateObstacle(A);
+        updateObstacle(B);
     }
 
     public void printEsc(){
@@ -70,6 +78,7 @@ public class Escape {
         System.out.println("Enter your move (W for up, S for down)");
         String WASD = scan.nextLine();
         WASD = WASD.toUpperCase();
+        playerInput++;
         if(WASD.equals("W")){
             if(row==0){
                System.out.println("Out of bounds");
@@ -92,5 +101,35 @@ public class Escape {
             System.out.println("Stayed still");
             hit = hitObstacle();
         }
+        if(playerInput==3){
+            insertObstacle(Y);
+        }
+        if(playerInput==5){
+            insertObstacle(Z);
+        }
+        if(playerInput==7){
+            insertObstacle(A);
+        }
+        if(playerInput==9){
+            insertObstacle(B);
+        }
+        if(playerInput<3){
+            updateObstacle(X);
+        }else if(playerInput<5){
+            updateObstacle(X);
+            updateObstacle(Y);
+        }else if(playerInput<7){
+            updateObstacle(X);
+            updateObstacle(Y);
+            updateObstacle(Z);
+        }else if(playerInput<9){
+            updateObstacle(X);
+            updateObstacle(Y);
+            updateObstacle(Z);
+            updateObstacle(A);
+        }else{
+            updateAllObstacles();
+        }
+
     }
 }
