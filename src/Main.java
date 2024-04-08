b0685226123107925e57e9539eb87dea98119fa8
cheesysamira <samiram18@nycstudents.net>
2024-04-08 01:54:58 -0400

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String [] args) {
-        MultiGamesRunner game1 = new MultiGamesRunner();
+        MultiGamesRunner game = new MultiGamesRunner();
     }
 }
