c3c7cb247365db2f24ef91a0ab2753a62c2cccba
cheesysamira <samiram18@nycstudents.net>
2024-04-07 22:59:45 -0400

    added and tried to debug

--- src/Ship.java
+++ src/Ship.java
@@ -1,13 +1,10 @@
 public class Ship {
     private String name;
     private int size;
-    private boolean isHorizontal;
-    private int[] position;
 
     public Ship(String name, int size) {
         this.name = name;
         this.size = size;
-        position = new int[size];
     }
 
     public String getName() {
@@ -16,21 +13,5 @@ public class Ship {
 
     public int getSize() {
         return size;
-    }
-
-    public boolean isHorizontal() {
-        return isHorizontal;
-    }
-
-    public void setHorizontal(boolean horizontal) {
-        isHorizontal = horizontal;
-    }
-
-    public int[] getPosition() {
-        return position;
-    }
-
-    public void setPosition(int[] position) {
-        this.position = position;
     }
 }
